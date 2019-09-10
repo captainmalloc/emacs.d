95dec35023e78709e56bbfa72a9bbf447014426d
Adriano Scoditti <adriano.scoditti@arm.com>
2019-09-10 01:45:54 -0500

    Adds elpa directory with el pacakges sources to ease first-time setup

---
+++ elpa/realgud-jdb-20190625.1911/realgud-jdb-pkg.el
@@ -0,0 +1,13 @@
+(define-package "realgud-jdb" "20190625.1911" "Realgud front-end to Java's jdb debugger\""
+  '((realgud "1.4.5")
+    (load-relative "1.2")
+    (cl-lib "0.5")
+    (emacs "25"))
+  :authors
+  '(("Rocky Bernstein" . "rocky@gnu.org"))
+  :maintainer
+  '("Rocky Bernstein" . "rocky@gnu.org")
+  :url "http://github.com/realgud/realgud-jdb")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/treemacs-magit-20190619.1516/treemacs-magit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "treemacs-magit" "20190619.1516" "Magit integration for treemacs" '((emacs "25.2") (treemacs "0.0") (pfuture "1.3") (magit "2.90.0")) :commit "ba34423a12bcdbf74e0afa4bd4bd5ff877251c08" :authors '(("Alexander Miller" . "alexanderm@web.de")) :maintainer '("Alexander Miller" . "alexanderm@web.de") :url "https://github.com/Alexander-Miller/treemacs")
